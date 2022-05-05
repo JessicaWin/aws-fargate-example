df3defa51c1cc855a1834f53117abf621ddf43c6
JessicaWin <baihechenlu@126.com>
2022-05-05 11:27:30 +0800

    Add aws resources: deploy bucket and ecr

---
+++ aws-resources/deploy-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DELETE=$1
+./deploy-for-each-stage.sh deploy-bucket $DELETE
+echo "waitting 60 seconds for deploy bucket create finish..."
+sleep 60
+./deploy-for-each-stage.sh ecr $DELETE
